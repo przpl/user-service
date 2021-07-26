9dbbe8595c201e59b749c10afbf5ac227c5b8a41
przpl <9073573+przpl@users.noreply.github.com>
2021-07-26 18:24:44 +0200

    test: Group tests

--- test/managers/session/cookieSessionManager.test.ts
+++ test/managers/session/cookieSessionManager.test.ts
@@ -37,132 +37,140 @@ describe("CookieSessionManager", () => {
         await testContainer.cleanup();
     }, 15000);
 
-    it("getUserIdFromSession() - not existing session should return null", async () => {
-        const userId = await sut.getUserIdFromSession("cookie1", "127.0.0.1");
-        expect(userId).toBeNull();
+    describe("getUserIdFromSession()", () => {
+        it("not existing session should return null", async () => {
+            const userId = await sut.getUserIdFromSession("cookie1", "127.0.0.1");
+            expect(userId).toBeNull();
+        });
+
+        it("should get session from db", async () => {
+            const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            await cacheDb.removeSession(sid);
+
+            const userIdFromDb = await sut.getUserIdFromSession(sid, "127.0.0.2");
+
+            expect(userIdFromDb).toBe("user1");
+            const session = await sessionRepo.findOne();
+            expect(session.lastRefreshIp).toBe("127.0.0.2");
+            expect(session.lastUseAt).toBeTruthy();
+            expect(await cacheDb.getSession(sid)).toBe("user1");
+        });
+
+        it("should get session from cache", async () => {
+            const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            await sessionRepo.clear();
+
+            const userIdFromCache = await sut.getUserIdFromSession(sid, "127.0.0.1");
+
+            expect(userIdFromCache).toBe("user1");
+            expect(await sessionRepo.findOne()).toBeFalsy();
+        });
     });
 
-    it("getUserIdFromSession() - should get session from db", async () => {
-        const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        await cacheDb.removeSession(sid);
+    describe("issueSession()", () => {
+        it("should issue session", async () => {
+            const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
 
-        const userIdFromDb = await sut.getUserIdFromSession(sid, "127.0.0.2");
+            const session = await sessionRepo.findOne();
+            expect(session.id).toHaveLength(SESSION_ID_LENGTH);
+            expect(session.userId).toBe("user1");
+            expect(session.createIp).toBe("127.0.0.1");
+            expect(session.lastRefreshIp).toBe("127.0.0.1");
+            const ua = mockUserAgent();
+            expect(session.browser).toBe(ua.browser);
+            expect(session.os).toBe(ua.os);
+            expect(session.osVersion).toBe(ua.osVersion);
+            expect(session.lastUseAt).toBeTruthy();
+            expect(await cacheDb.getSession(sid)).toBe("user1");
+        });
 
-        expect(userIdFromDb).toBe("user1");
-        const session = await sessionRepo.findOne();
-        expect(session.lastRefreshIp).toBe("127.0.0.2");
-        expect(session.lastUseAt).toBeTruthy();
-        expect(await cacheDb.getSession(sid)).toBe("user1");
-    });
-
-    it("getUserIdFromSession() - should get session from cache", async () => {
-        const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        await sessionRepo.clear();
-
-        const userIdFromCache = await sut.getUserIdFromSession(sid, "127.0.0.1");
-
-        expect(userIdFromCache).toBe("user1");
-        expect(await sessionRepo.findOne()).toBeFalsy();
-    });
-
-    it("issueSession() - should issue session", async () => {
-        const sid = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-
-        const session = await sessionRepo.findOne();
-        expect(session.id).toHaveLength(SESSION_ID_LENGTH);
-        expect(session.userId).toBe("user1");
-        expect(session.createIp).toBe("127.0.0.1");
-        expect(session.lastRefreshIp).toBe("127.0.0.1");
-        const ua = mockUserAgent();
-        expect(session.browser).toBe(ua.browser);
-        expect(session.os).toBe(ua.os);
-        expect(session.osVersion).toBe(ua.osVersion);
-        expect(session.lastUseAt).toBeTruthy();
-        expect(await cacheDb.getSession(sid)).toBe("user1");
-    });
-
-    it("issueSession() - should issue sessions with different ids", async () => {
-        const ids: string[] = [];
-        for (let i = 0; i < 5; i++) {
-            ids.push(await sut.issueSession("user1", "127.0.0.1", mockUserAgent()));
-        }
-
-        expect(new Set(ids).size).toBe(ids.length);
-    });
-
-    it("issueSession() - should respect sessions limit", async () => {
-        const config = mockConfig();
-        const ids: string[] = [];
-        for (let i = 0; i < config.session.maxPerUser + 2; i++) {
-            ids.push(await sut.issueSession("user1", "127.0.0.1", mockUserAgent()));
-        }
-
-        expect(await sessionRepo.count()).toBe(config.session.maxPerUser);
-        let sessionsInCache = 0;
-        for (const id of ids) {
-            if (await cacheDb.getSession(id)) {
-                sessionsInCache++;
+        it("should issue sessions with different ids", async () => {
+            const ids: string[] = [];
+            for (let i = 0; i < 5; i++) {
+                ids.push(await sut.issueSession("user1", "127.0.0.1", mockUserAgent()));
             }
-        }
-        expect(sessionsInCache).toBe(1);
+
+            expect(new Set(ids).size).toBe(ids.length);
+        });
+
+        it("should respect sessions limit", async () => {
+            const config = mockConfig();
+            const ids: string[] = [];
+            for (let i = 0; i < config.session.maxPerUser + 2; i++) {
+                ids.push(await sut.issueSession("user1", "127.0.0.1", mockUserAgent()));
+            }
+
+            expect(await sessionRepo.count()).toBe(config.session.maxPerUser);
+            let sessionsInCache = 0;
+            for (const id of ids) {
+                if (await cacheDb.getSession(id)) {
+                    sessionsInCache++;
+                }
+            }
+            expect(sessionsInCache).toBe(1);
+        });
     });
 
-    it("removeAllSessions() - should remove all user sessions", async () => {
-        const user = new UserEntity("user2", "username2");
-        await postgresConnection.getRepository(UserEntity).save(user);
-        const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        await sut.issueSession("user2", "127.0.0.1", mockUserAgent());
+    describe("removeAllSessions()", () => {
+        it("should remove all user sessions", async () => {
+            const user = new UserEntity("user2", "username2");
+            await postgresConnection.getRepository(UserEntity).save(user);
+            const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            await sut.issueSession("user2", "127.0.0.1", mockUserAgent());
 
-        await sut.removeAllSessions("user1");
+            await sut.removeAllSessions("user1");
 
-        expect(await sessionRepo.count()).toBe(1);
-        expect((await sessionRepo.findOne()).userId).toBe("user2");
-        expect(await cacheDb.getSession(sid1)).toBeNull();
-        expect(await cacheDb.getSession(sid2)).toBeNull();
+            expect(await sessionRepo.count()).toBe(1);
+            expect((await sessionRepo.findOne()).userId).toBe("user2");
+            expect(await cacheDb.getSession(sid1)).toBeNull();
+            expect(await cacheDb.getSession(sid2)).toBeNull();
+        });
+
+        it("should remove not existing user sessions", async () => {
+            const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+
+            await sut.removeAllSessions("user2");
+
+            expect(await sessionRepo.count()).toBe(1);
+            expect((await sessionRepo.findOne()).id).toBe(sid1);
+            expect(await cacheDb.getSession(sid1)).toBe("user1");
+        });
     });
 
-    it("removeAllSessions() - should remove not existing user sessions", async () => {
-        const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+    describe("removeSession()", () => {
+        it("should remove not cached session", async () => {
+            const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
 
-        await sut.removeAllSessions("user2");
+            const removed = await sut.removeSession(sid1);
 
-        expect(await sessionRepo.count()).toBe(1);
-        expect((await sessionRepo.findOne()).id).toBe(sid1);
-        expect(await cacheDb.getSession(sid1)).toBe("user1");
-    });
+            expect(removed.id).toBe(sid1);
+            expect(await sessionRepo.count()).toBe(1);
+            expect((await sessionRepo.findOne()).id).toBe(sid2);
+            expect(await cacheDb.getSession(sid2)).toBe("user1");
+        });
 
-    it("removeSession() - should remove not cached session", async () => {
-        const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+        it("should remove cached session", async () => {
+            const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
 
-        const removed = await sut.removeSession(sid1);
+            const removed = await sut.removeSession(sid2);
 
-        expect(removed.id).toBe(sid1);
-        expect(await sessionRepo.count()).toBe(1);
-        expect((await sessionRepo.findOne()).id).toBe(sid2);
-        expect(await cacheDb.getSession(sid2)).toBe("user1");
-    });
+            expect(removed.id).toBe(sid2);
+            expect(await sessionRepo.count()).toBe(1);
+            expect((await sessionRepo.findOne()).id).toBe(sid1);
+            expect(await cacheDb.getSession(sid1)).toBeNull;
+        });
 
-    it("removeSession() - should remove cached session", async () => {
-        const sid1 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        const sid2 = await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+        it("should not remove not existing session", async () => {
+            await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
+            await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
 
-        const removed = await sut.removeSession(sid2);
+            const removed = await sut.removeSession("sessionId");
 
-        expect(removed.id).toBe(sid2);
-        expect(await sessionRepo.count()).toBe(1);
-        expect((await sessionRepo.findOne()).id).toBe(sid1);
-        expect(await cacheDb.getSession(sid1)).toBeNull;
-    });
-
-    it("removeSession() - should not remove not existing session", async () => {
-        await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-        await sut.issueSession("user1", "127.0.0.1", mockUserAgent());
-
-        const removed = await sut.removeSession("sessionId");
-
-        expect(removed).toBeNull();
-        expect(await sessionRepo.count()).toBe(2);
+            expect(removed).toBeNull();
+            expect(await sessionRepo.count()).toBe(2);
+        });
     });
 });
